b5b8dd8130eba3f4b3e54ab0d3bf9fa31b54b957
Ana Paula Hiraiwa <hhiraiwa1@hotmail.com>
2025-08-14 14:16:00 -0300

    criando um servidor http

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const porta = 3000;
+
+const host = "0.0.0.0";
+//0.0.0.0 permite acesso à aplicação vindas de todas as interfaces de rede
+
+app.get("/dinheiro", (requisicao, resposta) => {
+  resposta.send(`<h1>R$${Math.random() * 1000}</h1>`);
+}); //rota para o endpoint /dinheiro
+
+app.listen(porta, host, () => {
+  console.log(`Servidor em execução em http://${host}:${porta}`); // template literals
+});
