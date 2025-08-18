24fe2e781857730a99d5a80c9fc30a6124b30bc8
Ana Paula Hiraiwa <hhiraiwa1@hotmail.com>
2025-08-18 08:47:33 -0300

    att home page

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ const porta = 3000;
 const host = "0.0.0.0";
 //0.0.0.0 permite acesso à aplicação vindas de todas as interfaces de rede
 
-app.get("/dinheiro", (requisicao, resposta) => {
-  resposta.send(`<h1>R$${Math.random() * 1000}</h1>`);
-}); //rota para o endpoint /dinheiro
+// app.get("/dinheiro", (requisicao, resposta) => {
+//   resposta.send(`<h1>R$${Math.random() * 1000}</h1>`);
+// }); //rota para o endpoint /dinheiro
+
+//configurando o servidor para prover arquivos estáticos
+app.use(express.static("/publico"));
+
 
 app.listen(porta, host, () => {
   console.log(`Servidor em execução em http://${host}:${porta}`); // template literals
